7b42905b785bd1122466a0c555114328a32ac09c
seeleytc <seeleytc@s.dcsdk12.org>
2021-03-04 09:39:09 -0700

    commit 2

--- Magic8ball/src/Magic8Ball.java
+++ Magic8ball/src/Magic8Ball.java
@@ -1,38 +1,20 @@
+java.util.Scanner;
+java.util.Random;
 public class Magic8Ball {
     public static void main(String[] args) {
         String[] outcomes =
-                //It is certain.
-                //● It is decidedly so.
-                //● Without a doubt.
-                //● Yes – definitely.
-                //● You may rely on it.
-                //
-                //● As I see it, yes.
-                //● Most likely.
-                //● Outlook good.
-                //● Yes.
-                //● Signs point to yes.
-                //
-                //● Reply hazy, try again.
-                //● Ask again later.
-                //● Better not tell you now.
-                //● Cannot predict now.
-                //● Concentrate and ask again.
-                //
-                //● Don't count on it.
-                //● My reply is no.
-                //● My sources say no.
-                //● Outlook not so good.
-                //● Very doubtful.
-                Scanner scan = new Scanner(System.in);
-        String question = scan.nextLine();
-        Random randNum = new Random();
-        Random randNum = nextInt(0, 2);
-        int result = randNum.nextInt( bound, 3);
-        System.out.println(outcomes[result]);
+                {"It is certain", "It is decidedly so.", "Without a doubt.", "Yes – definitely.", "You may rely on it.", "As I see it, yes.", "Most likely.", "Outlook good.", "Yes.", "Signs point to yes.", "Reply hazy, try again.", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.", "Don't count on it.", "My reply is no.", "My sources say no.", "Outlook not so good.", "Very doubtful"};
+        Scanner scan = new Scanner(System.in);
+        while (true) {
+            Random randNum = new Random();
+            int num = randNum.nextInt(20);
+            System.out.println("Any other questions? yes or no");
+            String answer = scan.nextLine();
+            if (answer.equalsIgnoreCase("no")) {
+                break;
+            }
 
 
-
-
+        }
     }
 }
